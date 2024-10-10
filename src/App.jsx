8b506419732cb5f97c25079aad4046b1ceac1b28
Sinomtha Mzamo <smzamo023@student.wethinkcode.co.za>
2024-10-10 15:25:51 +0200

    slowly constructing main frame layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -7,6 +7,7 @@ import Sidebar from './features/sidebar/SideBar'
 import View from './features/main/View'
 import Update from "./features/main/Update.jsx";
 import AddForm from './ui/AddForm.jsx';
+import Tab from "./features/sidebar/Tab.jsx";
 
 function App() {
   const [page, switchPage] = useState("");
@@ -45,8 +46,19 @@ function App() {
 
   return (
     <div className="">
+
       <h1 className='dancing-script'>Bhala edolweni...</h1>
-      <div style={{display:'flex', border: 'solid 2px wheat', padding: '0.9em', background:'lightgray'}} >
+      <div className="rect-wrapper"  style={{display: 'flex', justifyContent: 'space-evenly', alignItems: 'center'}}>
+        <div className="button-wrapper" style={{width: '200px'}}>
+          <Tab icon={"fas fa-add"} tabTitle={'Add New Debtor'} isCollapsed={false}
+               handleViewChange={() => updateView('add')}></Tab>
+        </div>
+        <form action="/search" method="GET">
+          <input type="search" name="query" placeholder="Search here..."/>
+          <button type="submit">Search</button>
+        </form>
+      </div>
+      <div style={{display: 'flex', border: 'solid 2px wheat', padding: '0.9em', background: 'lightgray'}}>
         <Sidebar updateView={updateView}/>
         {renderSelectedMainContent()}
       </div>
