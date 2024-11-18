2fbc709292a405c223872dff1b842496e2da05b4
Sinomtha Mzamo <smzamo023@student.wethinkcode.co.za>
2024-11-18 17:35:56 +0200

    Updated edit button to make the fields of the row editable upon click

--- frontend/GUI/src/main.jsx
+++ frontend/GUI/src/main.jsx
@@ -5,6 +5,7 @@ import './index.css'
 import Row from './ui/Row.jsx'
 import Table from './ui/Table.jsx'
 import View from './features/main/View.jsx'
+import ExampleComponent from './ui/ExampleComponent.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- frontend/GUI/src/ui/Row.jsx
+++ frontend/GUI/src/ui/Row.jsx
@@ -1,15 +1,40 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import IconButton from './IconButton';
 
 const Row = ({debtor, number}) => {
-    const [edit, remove, view] = ["fas fa-pencil", "fas fa-trash", "fas fa-info"];
-    const { id, name, amount, date} = debtor;
+    const [state, setState] = useState({
+        isEditing: false,
+        isDeleting: false,
+        isViewing: false,
+    })
+    
+    const [nameState, setNameState] = useState(debtor.name);
+    const [amountState, setAmountState] = useState(debtor.amount);
+
+    const nameInputRef = useRef();
+    const amountInputRef = useRef();
+
 
     const handleEdit = () => {
         // make the row name and amount fields editable
+        setState(prevState => ({
+            ...prevState, 
+            isEditing: true}));
+
         // disable other icons and show save button
         console.log('form is being edited!');
     }
+
+    // im gonna need these for clientside validation and making api calls
+    const handleNameInput = (e) => {
+        const newNameValue = e.target.value;
+        setNameState(newNameValue);
+    };
+
+    const handleAmountInput = (e) => {
+        const newAmountValue = e.target.value;
+        setAmountState(parseInt(newAmountValue));
+    };
 
     const handleRemoval = () => {
         // make row's outer boarder red
@@ -23,12 +48,50 @@ const Row = ({debtor, number}) => {
         console.log(debtor, 'showing debtor info');
     }
 
+
+    const NameInputField = () => {
+        return ( 
+            <input
+            ref={nameInputRef} 
+            type='text' 
+            defaultValue={nameState} 
+            />
+         );
+    }
+     
+    const AmountInputField = () => {
+        return(
+            <input
+            ref={amountInputRef} 
+            type="number" 
+            min='0' 
+            defaultValue={amountState} 
+            step="1"
+            />
+        );
+    };
+
+    const [edit, remove, view] = ["fas fa-pencil", "fas fa-trash", "fas fa-info"];
+    const { id, name, amount, date} = debtor;
+
     return ( 
         <>
             <tr>
                 <th scope='row'>{number}</th>
-                <td>{name}</td>
-                <td>R{amount}</td>
+                {state.isEditing ? (
+                    
+                    <td><NameInputField/></td>
+                    
+                ) : (
+                    <td>{name}</td>
+                )}
+                
+                {state.isEditing ? (
+                    <td><AmountInputField/></td>  
+                ) : (
+                    <td>R{amount}</td>
+                )}
+                
                 <td>{date}</td>
                 <td>
                     <div className="shortcuts" style={{display:'flex'}}>
